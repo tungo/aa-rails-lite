d43d2675f0eb9368cc183273ab7a2e4d41420404
Tu Ngo <tungo8810@gmail.com>
2017-04-25 14:29:15 -0700

    Update ControllerBase #render_content

--- lib/controller_base.rb
+++ lib/controller_base.rb
@@ -25,6 +25,8 @@ class ControllerBase
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    raise "Double Render" if already_built_response?
+    
     res.write(content)
     res['Content-Type'] = content_type
     @already_built_response = true
